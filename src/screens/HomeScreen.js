bad67f70c48a08b20f25628e5402d0799a4deb88
Hamzah Baig <mario@Hamzahs-MacBook-Pro.local>
2020-01-02 00:54:04 +0500

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return(
+            <Text>
+                Hello from home Screen!! 
+                </Text>
+        )
+    }
+}
